5457d408d0ea74229607a25867331c2ae11069de
B0022036 <133851352+B0022036@users.noreply.github.com>
2023-07-11 07:34:15 +0900

    Add files via upload

--- 最終課題/home/script.js
+++ 最終課題/home/script.js
@@ -1,4 +1,12 @@
 'use strict'
 
-window.alert('おいでませ山口！！！');
+//window.alert('おいでませ山口！！！');
 
+window.onload = function() {
+	const list = ["大吉", "吉", "中吉", "小吉", "末吉", "凶", "大凶"];
+
+	document.getElementById("omikuzi_button").onclick = function() {
+		var random = Math.floor(Math.random() * list.length);
+		document.getElementById("omikuzi_kekka").textContent = list [random];
+	}
+}
